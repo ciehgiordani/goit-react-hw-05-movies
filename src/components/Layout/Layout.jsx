9809e6d3c228975204fd68bfbe247c5ff9388b9e
ciehgiordani <acieh_giordani@yahoo.com>
2024-04-11 14:01:53 +0800

    initial commit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from 'react-router-dom';
+import { Container, Header, Logo, Link } from './Layout.styled';
+
+ const Layout = () => {
+  return (
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            🎞️
+          </span>{' '}
+          Movies Online Store
+        </Logo>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+      <Outlet />
+    </Container>
+  );
+};
+
+export default Layout;
